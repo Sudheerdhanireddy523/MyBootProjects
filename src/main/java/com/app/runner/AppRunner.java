12576c59e48a939d8f2621b6614c78aa2acdf7df
sudha <sudha@SalmaSudheer>
2019-06-07 11:38:29 +0530

    saved changes

---
+++ src/main/java/com/app/runner/AppRunner.java
@@ -0,0 +1,16 @@
+package com.app.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppRunner implements CommandLineRunner{
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+		
+	}
+
+}
